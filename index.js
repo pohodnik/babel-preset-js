89b5205c48e2d11c2efb972d603a7810e792df8d
pohodnik58 <pohodnik58@gmail.com>
2020-09-10 07:58:39 +0300

    env config

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const env = process.env.BABEL_ENV || process.env.NODE_ENV || 'development';
-/* eslint-disable global-require */
 module.exports = function () {
     return {
         presets: [
-            require('@pohodnik/babel-preset'),
-            [
-                require('@babel/preset-react').default,
-                {
-                    development: env !== 'production',
-                    useBuiltIns: true,
-                },
-            ],
+            '@pohodnik/babel-preset',
+            '@babel/preset-react'
         ],
-        plugins: [
-            env === 'production' && [
-                require('babel-plugin-transform-react-remove-prop-types').default,
-                { removeImport: true },
-            ],
-        ].filter(Boolean)
+        env: {
+            test: {
+                plugins: []
+            },
+            development: {
+                plugins: []
+            },
+            production: {
+                plugins: ['babel-plugin-transform-react-remove-prop-types', { removeImport: true }]
+            }
+        }
     };
 };
